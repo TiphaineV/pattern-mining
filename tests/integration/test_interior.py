eadb91cdc4a84858ef3309eb418bbf2a22be12d5
Tiphaine Viard <tiphaine.viard@riken.jp>
2020-04-23 19:34:31 +0200

    Added interior tests on new datafiles (separate tests for now)

--- tests/integration/test_interior.py
+++ tests/integration/test_interior.py
@@ -5,6 +5,7 @@ from lib.StreamProperties import StreamStarSat
 from lib.patterns import interior
 import logging
 import os
+import ujson
 
 
 class TestInterior:
@@ -36,3 +37,23 @@ class TestInterior:
         assert(int_val == expected)
 
 
+    @pytest.mark.datafiles(
+        os.path.join(FIXTURE_DIR, "ChangingNeighbours-StSa-Copy1.json")        
+    )
+    def test_interior_changing_neighbours(self, datafiles):
+        s = Stream(lang=set("abcd"), _loglevel=logging.INFO)
+        core_property = StreamStarSat(s, threshold=2)
+        s.setCoreProperty(core_property)
+        s.readStream(datafiles.listdir()[0])
+
+        X1 = [TimeNode(x["u"], x["b"], x["e"]) for x in s.E ]
+        X2 = [TimeNode(x["v"], x["b"], x["e"]) for x in s.E ]
+
+        X = X1 + X2
+        X = TimeNodeSet(elements=X)
+        s.W = X
+        int_val = interior(s, X, X)
+
+        expected = (TimeNodeSet(elements=[TimeNode('v', 1, 4)]),\
+                    TimeNodeSet(elements=[TimeNode('u', 1, 4), TimeNode('y', 2, 4), TimeNode('x', 1, 3)]))
+        assert(int_val == expected)
