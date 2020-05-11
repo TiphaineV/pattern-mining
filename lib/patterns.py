a80d37d1a1bd2106200c689a14701aab80eedb5f
Tiphaine Viard <tiphaine.viard@riken.jp>
2020-05-11 19:35:04 +0200

    Finished BiPattern class and made generic enum() function

--- lib/patterns.py
+++ lib/patterns.py
@@ -67,31 +67,36 @@ class BiPattern:
     """
         Bipattern class
     """
-    def __init__(self, _lang=(set(), set()), _support_set=set()):
+    def __init__(self, _lang={ "left": set(), "right": set() }, _support_set=set()):
         self.lang = _lang
         self.support_set = _support_set
     
     def add(self, item, side):
         if side == 0:
-            self.lang[0].add(item)
+            self.lang["left"].add(item)
         else:
-            self.lang[1].add(item)
+            self.lang["right"].add(item)
         
     def minus(self, I):
-        return set([ x for x in set(I[0]).difference(self.lang[0]) ]), set([ x for x in set(I[1]).difference(self.lang[1]) ])
+        return set([ x for x in set(I["left"]).difference(self.lang["left"]) ]), set([ x for x in set(I["right"]).difference(self.lang["right"]) ])
+    
+    def elements(self):
+        return self.lang["left"].union(self.lang["right"])
     
     def intent(self):
         """
             Returns the intent of a pattern
         """
         
-        langs_left = [ self.support_set.label(x) for x in self.support_set.W.values() if x.node in self.support_set._top ]
-        langs_right = [ self.support_set.label(x) for x in self.support_set.W.values() if x.node in self.support_set._bot ]
+        langs_left = [ self.support_set.label(x) for x in self.support_set.W.values() if x.node in self.support_set.V["left"] ]
+        langs_right = [ self.support_set.label(x) for x in self.support_set.W.values() if x.node in self.support_set.V["right"] ]
         
-        if langs == []:
-            langs = [set()]
+        if langs_left == []:
+            langs_left = [set()]
+        if langs_right == []:
+            langs_right = [set()]
 
-        return set.intersection(*langs)
+        return { "left": set.intersection(*langs_left), "right": set.intersection(*langs_right) }
 
     def extent(self, S=None):
         """
@@ -104,15 +109,21 @@ class BiPattern:
         else:
             S = (S.E, S.E)
             
-        X1 = [ TimeNode(x["u"], x["b"], x["e"]) for x in S[0] if q.issubset(set(x["label_u"]))]
-        X2 = [ TimeNode(x["v"], x["b"], x["e"]) for x in S[1] if q.issubset(set(x["label_v"]))]
+        X1 = [ TimeNode(x["u"], x["b"], x["e"]) for x in S[0] if q["left"].issubset(set(x["label"]["left"]))]
+        X2 = [ TimeNode(x["v"], x["b"], x["e"]) for x in S[1] if q["right"].issubset(set(x["label"]["right"]))]
         
         X = X1 + X2
         X = TimeNodeSet(elements=X)
         return X
     
+    def copy(self):
+        """
+            Returns a copy of the current BiPattern object.
+        """
+        return BiPattern(copy.deepcopy(self.lang), self.support_set.copy())
+    
     def __str__(self):
-        return f"{self.lang} {self.support_set.W}"
+        return f"{self.lang['left']}, {self.lang['right']} {self.support_set.W}"
         
     def __repr__(self):
         return self.__str__()
@@ -205,7 +216,7 @@ def get_stars_sats(s, threshold=1):
     return stars, satellites
 
 
-def bipatterns(stream, _top, _bot, s=2):
+def bipatterns(stream, s=2):
     """
         Enumerates all bipatterns
     """
@@ -213,11 +224,12 @@ def bipatterns(stream, _top, _bot, s=2):
     stream.bipatterns_list = []
     # S = interior(self, _top, _bot, set())
     S = stream.core_property.interior(stream)
-    pattern = Pattern(set(), S)
+#     pattern = Pattern(set(), S)
+    pattern = BiPattern({ "left": set(), "right": set() }, S)
     pattern.lang = pattern.intent() # Pattern(stream.intent([stream.label(x) for x in stream.W.values()]),S)
-    enum(stream, pattern, set(), s=s, glob_stream=stream)
+    enum(stream, pattern, set(), s=s, glob_stream=stream, patternClass=BiPattern)
 
-def patterns(stream, _top, _bot, s=2):  
+def patterns(stream, s=2):  
     """
         Enumerates all patterns
     """
@@ -225,14 +237,17 @@ def patterns(stream, _top, _bot, s=2):
     stream.bipatterns_list = []
     # S = interior(self, _top, _bot, set())
     S = stream.core_property.interior(stream)
-    pattern = BiPattern(set(), S)
+    pattern = Pattern(set(), S)
     pattern.lang = pattern.intent() # Pattern(stream.intent([stream.label(x) for x in stream.W.values()]),S)
-    enum(stream, pattern, set(), s=s, glob_stream=stream)
+    enum(stream, pattern, set(), s=s, glob_stream=stream, patternClass=Pattern)
 
-def enum(stream, pattern, EL=set(), depth=0, s=2, parent=set(), glob_stream=None):
+def enum(stream, pattern, EL=set(), depth=0, s=2, parent=set(), glob_stream=None, patternClass=None):
     """
-        Internal routine for pattern enumeration
+        Internal routine for pattern enumeration.
+        Should not be called outside of patterns/bipatterns ?
     """
+    
+    # patternClass = BiPattern
     
     # Pretty print purposes
     prefix = depth * 4 * ' '
@@ -245,48 +260,55 @@ def enum(stream, pattern, EL=set(), depth=0, s=2, parent=set(), glob_stream=None
     # print(f"{q} {S}", file=stream.bip_fp)
     
     # Rewrite this in minus
+    # XXX
     lang = S.I
     lang = [ S.label(x) for x in S.W.values() ]
-    lang = [item for sublist in lang for item in list(sublist) if item not in q  and item not in S.EL ]
+    lang = [item for sublist in lang for item in list(sublist) if item not in pattern.elements() and item not in S.EL ]
     candidates = set(lang)
     
     # bak variables are necessary so that deeper recursion levels do not modify the current object
-    # Could be done by copy() in call ?
-    q_bak = q.copy()
-    S_bak = S.copy() # (S[0].copy(), S[1].copy())
-#         EL_bak = EL.copy()
-    
+    pattern_bak = pattern.copy()
+
     for x in candidates:
         # S is not reduced between candidates at the same level of the search tree
-        S = S_bak.copy() # (S_bak[0].copy(), S_bak[1].copy())
-        q_x = q_bak.copy()
+        pattern_x = pattern_bak.copy()
+        S = pattern_bak.support_set.copy() # (S_bak[0].copy(), S_bak[1].copy())
 
         # Add candidate to pattern
-        q_x.add(x)
-        p_x = Pattern(q_x, S)
-        # print(q_x)
+        if patternClass is BiPattern:
+            # If Bipattern we need to identify the side to which the candidate belongs
+            if x in stream.I["left"]:
+                side = 0
+            elif x in stream.I["right"]:
+                side = 1
+            else:
+                raise LanguageError("aha", "Element was not found in language set(s).")
+            pattern_x.add(x, side)
+        else:
+            # (mono)Pattern case
+            pattern_x.add(x)
+
         # Support set of q_x
-        X = p_x.extent(S=S)
+        X = pattern_x.extent(S=S)
 
         S_x = (X.intersection(S.W), X.intersection(S.W))
-        # print(f"S_x: {S_x}")
+
         subs = stream.substream(S_x[0], S_x[1])
         subs.setCoreProperty(stream.core_property)
         subs.EL = stream.EL
         # S_x = interior(self, S_x[0], S_x[1], q_x) # p(S\cap ext(add(q, x)))
         S_x = subs.core_property.interior(subs)
-        p_x = Pattern(q_x, S_x)
-        # print(depth, len(stream.V), len(subs.V))
-        # ipdb.set_trace()
-        #print(f'q_x={q_x} has support set S_x = {S_x}')
+        p_x = patternClass(pattern_x.lang, S_x)
+
         if len(p_x.support_set.W) >= s:
             p_x.lang = p_x.intent() # S_x.intent([ S_x.label(x) for x in S_x.W.values() ])
-            
-            if len(p_x.lang.intersection(stream.EL)) == 0 and p_x.lang != q: #(q_x != q and not (S_x[0] == S[0] and S_x[1] == S[1])):                     
+            langs = p_x.elements().intersection(stream.EL)
+
+            if len(langs) == 0 and p_x.lang != q: #(q_x != q and not (S_x[0] == S[0] and S_x[1] == S[1])):                     
                 # pattern_x = Pattern(q_x, S_x)
 
                 # print(f"{prefix} Calling enum with {pattern_x.lang} ({S_x})")
-                enum(subs, p_x, stream.EL, depth+1, parent=q, glob_stream=glob_stream)
+                enum(subs, p_x, stream.EL, depth+1, parent=q, glob_stream=glob_stream, patternClass=patternClass)
                 
                 # We reached a leaf of the recursion tree, add item to exclusion list
                 # print(f"{prefix} Adding {x} to EL")
