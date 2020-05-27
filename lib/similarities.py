159360d12529d174bbe87c3c70d2fdb4af2a17b8
Tiphaine Viard <viard@lipn.univ-paris13.fr>
2020-05-27 09:47:10 +0200

    Added bipattern_similarity function

--- lib/similarities.py
+++ lib/similarities.py
@@ -20,3 +20,22 @@ def jaccard(s, u, v):
     inter = sum( ( x.e - x.b for x in n_u.intersection(n_v) ) )
 
     return inter / union
+
+def bipattern_similarity(s, p, q):
+    """
+        s: the stream graph on which the patterns are enumerated
+        p,q: two Patterns or BiPatterns
+        @return: the similarity between two patterns.
+    """
+    
+    top, bot = s.V["left"], s.V["right"]
+    
+    get_w = lambda p: (TimeNodeSet([x for x in p.support_set.W if x.node in top]), TimeNodeSet([x for x in p.support_set.W if x.node in bot]))
+    
+    W1 = get_w(p)
+    W2 = get_w(q)
+    
+    score_top = len(W1[0].intersection(W2[0])) / len(W1[0].union(W2[0]))
+    score_bot = len(W1[1].intersection(W2[1])) / len(W1[1].union(W2[1]))
+    
+    return 1 - min(score_top, score_bot)
